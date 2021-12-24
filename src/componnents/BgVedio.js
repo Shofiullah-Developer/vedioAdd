c4772944df5ef8c4af78eb3af256efddce950c96
Shofiullah-Developer <shofiullah1762@gmail.com>
2021-12-24 18:39:30 +0600

    vedio add react appt

---
+++ src/componnents/BgVedio.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button, Col, Row } from 'react-bootstrap'
+import video from './video/video.mp4'
+
+
+function BgVedio() {
+    return (
+        <div style={{position: 'relative'}}>
+            <video autoPlay loop muted 
+                src= {video} type="video/mp4"
+            style={{
+                position: "absolute",
+                width: "100%",
+                left:"50%",
+                top:"50%",
+                height: "85vh",
+                objectFit:"cover",
+                transform:'translate(-50%, -50%)',
+                 zIndex:'-1',
+                 
+            }}
+            >
+               
+            </video> 
+            <div style={{ display:'flex', justifyContent: 'center', alignItems:'center',height:'85vh'}}>
+            <Row style={{ display:'flex', justifyContent: 'center', alignItems:'center'}}>
+                  <Col  xs={12} sm={12} md={10}
+                  style={{display:'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center'}}
+                  >
+                      <h1 style={{fontWeight: 'bold', color: 'white' }}>The Best Place to Collect NFs</h1>
+                      <p className="py-3" style={{width: '46%', textAlign: 'center', color: 'white'}}>
+                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo doL
+                      </p>
+                      <div>
+                        <Button className=" text-light me-2 ps-4 pe-4 fw-bold "variant="info">Create</Button>{' '}
+                        <Button className="text-light fw-bold "variant="outline-info">Connect Wallet</Button>{' '}
+                      </div>
+                  </Col>
+                </Row>
+            </div>
+                
+        </div>
+    )
+}
+
+export default BgVedio
